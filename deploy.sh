680869dca67b26da96b9bb07092998f8842cb3c0
yutarot <takenoco88@gmail.com>
2018-08-08 21:42:50 +0900

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,7 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo yutarot.me >> CNAME
+echo yutarot.me >> public/CNAME
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
